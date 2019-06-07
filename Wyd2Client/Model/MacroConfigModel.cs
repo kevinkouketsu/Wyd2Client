cc392109ea23db2a0f1d6cf26821e625d6733b43
Kevin Kouketsu <kevinkouketsu53@gmail.com>
2019-06-07 17:48:44 -0300

    Iniciado janela de configuração do Macro

---
+++ Wyd2Client/Model/MacroConfigModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyd2.Client.Model
+{
+    public class MacroConfigModel
+    {
+        public bool IsContinous { get; set; }
+        public bool IsFixed { get; set; }
+    }
+}
---
+++ Wyd2Client/ViewModel/MacroConfigViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wyd2.Client.Model;
+
+namespace Wyd2.Client.ViewModel
+{
+    public class MacroConfigViewModel : BaseViewModel
+    {
+        private MacroConfigModel Model = new MacroConfigModel();
+
+        public bool IsFixed
+        {
+            get => Model.IsFixed;
+            set
+            {
+                Model.IsFixed = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsContinous
+        {
+            get => Model.IsContinous;
+            set
+            {
+                Model.IsContinous = value;
+
+                OnPropertyChanged();
+            }
+        }
+    }
+}
